4eb3966cb6e88bb051229878a013bc666917eb26
League Student <league@iMac-27.attlocal.net>
2020-02-04 18:29:10 -0800

    Made level one text and wizard/wizard movement:smile:

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -14,20 +14,34 @@ public class GamePanel extends JPanel implements KeyListener{
 	public static boolean needImage = true;
 	public static boolean gotImage = false;
 	
+	public BufferedImage wizardimage1;
+	
 	final int MENU = 0;
     final int GAME = 1;
     final int BOSS = 2;
     final int END = 3;
     int mode = MENU;
+    int level = 1;
 	
 	String menuBackground = "menu_background.jpg";
 	String gameBackground = "grass_14.png";
+	String wizard1 = "wandless_wizard.png";
 	Font welcomeFont = new Font("Arial", Font.PLAIN, 50);
 	Font continueFont = new Font("Arial", Font.PLAIN, 40);
+	Font gameFont = new Font("Arial", Font.PLAIN, 40);
+	Font gameSubtitle = new Font("Arial", Font.BOLD, 25);
+	int wizardX = 50;
+	int wizardY = 450;
+	
 	
 	GamePanel() {
 		if (needImage) {
 		    loadImage (menuBackground);
+		    try {
+			    wizardimage1 = ImageIO.read(this.getClass().getResourceAsStream(wizard1));
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 		}
 	}
 	
@@ -43,6 +57,16 @@ public class GamePanel extends JPanel implements KeyListener{
 		if(mode==MENU) {
 			drawMenuText(g);
 		}
+		if(mode==GAME) {
+			setGameText(g);
+			g.drawImage(wizardimage1, wizardX, wizardY, 100, 100, null);
+			if(level==1) {
+				drawLevel1Text(g);
+			}
+			
+			
+			
+		}
 	}
 	
 	void drawMenuText(Graphics g) {
@@ -53,11 +77,21 @@ public class GamePanel extends JPanel implements KeyListener{
 		g.drawString("Press Space to Play", 300, 500);
 	}
 	
+	void setGameText(Graphics g) {
+		g.setFont(gameFont);
+		g.setColor(new Color(0, 0, 50));
+	}
+	
+	void drawLevel1Text(Graphics g) {
+		g.drawString("Hello, stranger. Welcome to this bizarre land.", 125, 100);
+		g.setFont(gameSubtitle);
+		g.drawString("Use arrow keys to move.", 350, 150);
+	}
 	
 	void loadImage(String imageFile) {
 	    if (needImage) {
 	        try {
-	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+	        	image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
 		    gotImage = true;
 	        } catch (Exception e) {
 	            
@@ -79,6 +113,18 @@ public class GamePanel extends JPanel implements KeyListener{
 			mode=GAME;
 		    needImage=true;
 			loadImage (gameBackground);
+		}
+		if(e.getKeyCode()==KeyEvent.VK_UP) {
+			wizardY-=10;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+			wizardY+=10;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			wizardX-=10;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			wizardX+=10;
 		}
 		repaint();
 	}
